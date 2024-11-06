9f32436992a6393cff09de6899ab2ad8d79cedc0
Gev <gevorg.markosyan@telecomarmenia.am>
2024-11-06 13:58:19 +0400

    add init and variabels failes

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "TerraformInstance"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+
+
+resource "aws_instance" "web" {
+  ami              = "ami-0866a3c8686eaeeba"
+  instance_type    = var.instance_type
+  user_data_base64 = base64encode(file("init.sh"))
+  tags = {
+    Name = var.tag_name
+  }
 }
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "instance_type" {
+  description = "value"
+  type        = string
+  default     = "t2.micro"
+}
+variable "tag_name" {
+  description = "value"
+  type        = string
+  default     = "TerraformInstance"
+}
