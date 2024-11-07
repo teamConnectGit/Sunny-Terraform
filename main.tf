cd68a33b0a7302ff366d84ad01425c219aff89da
Gev <gevorg.markosyan@telecomarmenia.am>
2024-11-07 17:16:55 +0400

    create vpc, subnet, internet gateway,secure groupe

--- main.tf
+++ main.tf
@@ -1,34 +1,108 @@
-# data "aws_ami" "ubuntu" {
-#   most_recent = true
 
-#   filter {
-#     name   = "name"
-#     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-#   }
-
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
-#   owners = ["099720109477"] # Canonical
-# }
-
-
-
-resource "aws_instance" "web_server" {
-  ami           = "ami-0866a3c8686eaeeba"
-  instance_type = var.instance_type
-  # key_name         = "terraform"
+resource "aws_instance" "web_server1" {
+  ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = var.instance_type
+  key_name               = "Terraform-Key"
   user_data_base64       = base64encode(file("init.sh"))
-  vpc_security_group_ids = [aws_security_group.web_server_sg.id]
+  subnet_id              = aws_subnet.public_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_server_public_sg.id]
   tags = {
-    Name = var.tag_name
+    Name = "${var.tag_name}-public"
   }
 }
 
+resource "aws_instance" "web_server2" {
+  ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = var.instance_type
+  key_name               = "Terraform-Key"
+  user_data_base64       = base64encode(file("init.sh"))
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.web_server_private_sg.id]
+  tags = {
+    Name = "${var.tag_name}-private"
+  }
+}
+
+#Define VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+#  Define Subnets
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+#Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+#Public route table
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "public-rt"
+  }
+}
+#Private route table
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "private-rt"
+  }
+}
+
+#Associate the public subnet with the public route
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
 # Security Group
-resource "aws_security_group" "web_server_sg" {
-  name        = "web-server-sg"
+resource "aws_security_group" "web_server_public_sg" {
+  vpc_id      = aws_vpc.main.id
+  name        = "web-server-public-sg"
   description = "Security group for web server"
 
   # Ingress rules (incoming traffic)
@@ -53,8 +127,41 @@ resource "aws_security_group" "web_server_sg" {
     protocol    = "-1" # Allow all outbound traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = {
+    Name = "web-server-public-sg"
+  }
+}
+resource "aws_security_group" "web_server_private_sg" {
+  vpc_id      = aws_vpc.main.id
+  name        = "web-server-private-sg"
+  description = "Security group for web server"
+
+  ingress {
+    from_port       = -1
+    to_port         = -1
+    protocol        = "icmp"
+    security_groups = [aws_security_group.web_server_public_sg.id]
+  }
+
+  # Ingress rules (incoming traffic)
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web_server_public_sg.id]
+    # cidr_blocks = ["10.0.1.0/24"] # Allow SSH from any IP address (use carefully)
+  }
+
+
+  # Egress rules (outgoing traffic)
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # Allow all outbound traffic
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
-    Name = "web-server-sg"
+    Name = "web-server-private-sg"
   }
 }
